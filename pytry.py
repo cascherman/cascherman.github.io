380946726fa654fd1e2187f16662113dcdcf01ac
cascherman <63865013+cascherman@users.noreply.github.com>
2024-08-17 00:33:14 -0400

    Update pytry.py

--- pytry.py
+++ pytry.py
@@ -1,1 +1,29 @@
-print("hello world")
+problem = 0
+number1 = ''
+number2 = ''
+sign = ''
+
+def entry():
+    number1 = input('first number')
+    sign = input ('what to do')
+    number2 = input ('second number')
+    which_sign()
+
+def which_sign():
+    if sign == '+':
+        add()
+    elif sign == '-':
+        subtract ()
+    elif sign == '*':
+        multiply()
+    elif sign == '/':
+        divide ()
+    else:
+        entry()
+        
+
+            
+
+        
+    
+    
